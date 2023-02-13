843a44e450d660008dcbb137bbfe6c34f6754c25
Jannatul-1337 <jannatulferdous31999@gmail.com>
2023-02-14 01:02:28 +0600

    1st

---
+++ BioData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.biodata;
+
+/**
+ *
+ * @author Hp
+ */
+public class BioData {
+
+    public static void main(String[] args) {
+       System.out.println("Jannatul Ferdous");
+        System.out.println("ID:221-15-5624");
+        System.out.println("Saidpur,Nilfamari");
+        System.out.println("Department Of CSE");
+        System.out.println("Badminton");
+    }
+}
