a1dc774d305a3633692f27d28f600a8b07f374e6
Jannatul-1337 <jannatulferdous31999@gmail.com>
2023-02-14 01:06:49 +0600

    4th

---
+++ AreaCircule.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.areacircule;
+
+/**
+ *
+ * @author Hp
+ */
+public class AreaCircule {
+
+    public static void main(String[] args) {
+         double pi =3.1416,area;
+        int r=4;
+        area=pi*r*r;
+        System.out.printf("Area of Circle is : %.2f\n", area);
+    }
+}
