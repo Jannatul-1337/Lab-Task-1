17370af78d53c47c48e908565eaf14b4f9c26aee
Jannatul-1337 <jannatulferdous31999@gmail.com>
2023-02-14 01:04:59 +0600

    3rd

---
+++ Name.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.name;
+
+/**
+ *
+ * @author Hp
+ */
+public class Name {
+
+    public static void main(String[] args) {
+        String Name =("Jannatul Ferdous");
+        System.out.println(Name);
+    }
+}
